33ebd6696aba2c312d86f7605a5800550afb5e5d
Ori <ori.n.1621@gmail.com>
2016-11-02 00:01:40 +0200

    v0.2.6 plugin improved, now applied before compilation

--- variantsstubsgenerator-plugin/src/main/groovy/com/oridev/variantsstubsgenerator/plugin/AddGeneratedSourcePlugin.groovy
+++ variantsstubsgenerator-plugin/src/main/groovy/com/oridev/variantsstubsgenerator/plugin/AddGeneratedSourcePlugin.groovy
@@ -10,6 +10,137 @@ public class AddGeneratedSourcePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        addPathsForVariants(project);
+    }
+
+    void addPathsForVariants(Project project) {
+
+        project.android.applicationVariants.all { variant ->
+
+            variant.javaCompile.doFirst {
+
+                def filesJson = new File(getJsonPath(project))
+                if (filesJson.exists()) {
+                    def filesEntries = new JsonSlurper().parse(filesJson);
+
+                    def variantPaths = filesEntries
+                            .findAll { variant.name.toLowerCase().contains(it.flavor) }
+                            .collect { it.path };
+
+                    if (!variantPaths.isEmpty()) {
+
+                        logMessage("applying AddGeneratedSourcePlugin for variant $variant.name with $variantPaths.size entries)..", true);
+                        variantPaths.each { path ->
+                            variant.javaCompile.source path;
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+
+
+    /* working!! */
+//    void addTargetsForVariants(Project project) {
+//
+//        def filesJson = new File(getJsonPath(project))
+//        if (filesJson.exists()) {
+//            def filesEntries = new JsonSlurper().parse(filesJson);
+//
+//            logMessage("applying AddGeneratedSourcePlugin, (info json containing $filesEntries.size entries)..", true);
+//
+//            project.android.applicationVariants.all { variant ->
+//
+//                addSourceFilesForVariant(project, variant, filesEntries);
+//                //addSourceDirectoriesForVariant(project, variant, filesEntries);
+//            }
+//        }
+//    }
+
+//    void addSourceFilesForVariant(def variant) {
+//
+//        def variantPaths = filesEntries
+//                .findAll { variant.name.toLowerCase().contains(it.flavor) }
+//                .collect { it.path };
+//
+//        variantPaths.each { path ->
+//
+//            def javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
+//            javaCompile.doFirst {
+//                logMessage("adding path [$path] to variant [$variant.name]", true);
+//                javaCompile.source path;
+//            }
+//
+//        }
+//
+//    }
+
+//    void addSourceFilesForVariant(def variant) {
+//
+//        def variantPaths = filesEntries
+//                .findAll { variant.name.toLowerCase().contains(it.flavor) }
+//                .collect { it.path };
+//
+//        variantPaths.each { path ->
+//
+//            def javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
+//            javaCompile.doFirst {
+//                logMessage("adding path [$path] to variant [$variant.name]", true);
+//                javaCompile.source path;
+//            }
+//        }
+//    }
+
+    /* not working, but starting to get there... */
+//    void addSourceDirectoriesForVariant(def project, def variant, def filesEntries) {
+//
+//        def variantTargets = filesEntries
+//        // extract entries targets
+//                .collect { it.flavor }
+//        // remove duplicates
+//                .unique()
+//        // filter only targets relevant for this variant
+//                .findAll { variant.name.toLowerCase().contains(it) };
+//
+//        variantTargets.each { target ->
+//
+//            def sourcePath = new File(getSourcePath(project, target, variant.buildType.name));
+//
+//            def javaCompile = variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
+//            javaCompile.doFirst {
+//                logMessage("adding path [$sourcePath] to variant [$variant.name]", true);
+//
+//                //javaCompile.source sourcePath;
+//
+//                variant.addJavaSourceFoldersToModel(sourcePath);
+//                javaCompile.options.compilerArgs += [
+//                        '-sourcepath', sourcePath,
+//                        //'-processorpath', configurations.apt.getAsPath()
+//                ]
+//
+//                logMessage("*** doFirst: javaCompile.compilerArgs: " + javaCompile.options.compilerArgs);
+////                        javaCompile.source.each {
+////                            logMessage("*** doFirst: javaCompile.source: " + it);
+////                        }
+//
+//            }
+//        }
+//    }
+
+//    static String getSourcePath(Project project, String target, String buildType) {
+//        def sourcePathStr = "${project.buildDir}/generated/source/apt/";
+//        if (["debug", "release"].contains(target)) {
+//            sourcePathStr += "main/${target}"
+//        } else {
+//            sourcePathStr += "${target}/${buildType}"
+//        }
+//        return sourcePathStr;
+//    }
+
+    /* Working code!! */
+    void addSourceFromJson(Project project) {
 
         def filesJson = new File(getJsonPath(project))
         if (filesJson.exists()) {
@@ -19,54 +150,31 @@ public class AddGeneratedSourcePlugin implements Plugin<Project> {
             filesPaths.each { generatedEntry ->
                 def entryTarget = generatedEntry.flavor;
 
-                if (["debug", "release"].contains(entryTarget)) {
-                    // if flavorTo is a buildType
-                    logMessage("handling entry with buildType target [$entryTarget], path [$generatedEntry.path]");
+                try {
+                    if (["debug", "release"].contains(entryTarget)) {
+                        // if flavorTo is a buildType
+                        logMessage("handling entry with buildType target [$entryTarget], path [$generatedEntry.path]");
 
-                    project.android {
-                        sourceSets {
-                            buildTypes.each { buildType ->
-                                try {
-                                    // if this generated file should be added to current flavor
-                                    if (entryTarget.equals(buildType.name)) {
-                                        logMessage("adding generated source [$generatedEntry.path] to buildType [$entryTarget]", true);
-                                        // add generated file path to flavor source set
-                                        "${entryTarget}" {
-                                            java.srcDirs += new File(generatedEntry.path)
-                                        }
-                                    }
+                        project.android.buildTypes.all { buildType ->
+                            // if this generated file should be added to current buildType
+                            if (entryTarget.equals(buildType.name)) {
+                                addSourceFileToSourceSet(project, entryTarget, generatedEntry.path)
+                            }
+                        }
 
-                                } catch (Exception e) {
-                                    println(e);
-                                }
+                    } else {
+                        // if flavorTo is a flavor
+                        logMessage("handling entry with flavor target [$entryTarget], path [$generatedEntry.path]");
+
+                        project.android.productFlavors.all { flavor ->
+                            // if this generated file should be added to current flavor
+                            if (entryTarget.equals(flavor.name)) {
+                                addSourceFileToSourceSet(project, entryTarget, generatedEntry.path);
                             }
                         }
                     }
-
-                } else {
-                    // if flavorTo is a flavor
-                    logMessage("handling entry with flavor target [$entryTarget], path [$generatedEntry.path]");
-
-                    project.android {
-                        sourceSets {
-                            productFlavors.each { flavor ->
-                                try {
-                                    // if this generated file should be added to current flavor
-                                    if (entryTarget.equals(flavor.name)) {
-                                        logMessage("adding generated source [$generatedEntry.path] to flavor [$entryTarget]", true);
-                                        // add generated file path to flavor source set
-                                        "${entryTarget}" {
-                                            java.srcDirs += new File(generatedEntry.path)
-                                        }
-                                    }
-
-                                } catch (Exception e) {
-                                    println(e);
-                                }
-                            }
-                        }
-                    }
-
+                } catch (Exception e) {
+                    println(e);
                 }
             }
         }
@@ -116,6 +224,17 @@ public class AddGeneratedSourcePlugin implements Plugin<Project> {
 //        }
     }
 
+    void addSourceFileToSourceSet(Project project, String target, String path) {
+        logMessage("adding generated source [$path] to target [$target]", true);
+        project.android {
+            sourceSets {
+                "${target}" {
+                    java.srcDirs += path
+                }
+            }
+        }
+    }
+
     static String getJsonPath(Project project) {
         return "$project.buildDir/generated/assets/variantsStubsGenerator/meta/generated_files.json";
 
@@ -125,7 +244,7 @@ public class AddGeneratedSourcePlugin implements Plugin<Project> {
     }
 
 
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
     static void logMessage(String msg) {
         logMessage(msg, false);
     }
